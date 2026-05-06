b6abfb6d9d579bc7ac7a571dcdfe339879c44d1d
nicholas53-s <nicholasyap53@gmail.com>
2026-05-06 14:54:48 +0700

    membuat fungsi searchbook untuk mencari buku berdasarkan judul.

--- src/functions/bookManager.ts
+++ src/functions/bookManager.ts
@@ -1,28 +1,9 @@
 // Tugas 3: Implementasikan fungsi-fungsi manajemen buku
 
-// Fungsi addBook
-// Fungsi ini digunakan untuk menambahkan buku baru ke dalam koleksi
-// Parameter yang dibutuhkan: data buku sesuai tipe Book
-// Fungsi ini tidak mengembalikan nilai (void)
-// Petunjuk: pikirkan bagaimana cara menambahkan buku ke array yang sudah disediakan
-
-// Fungsi listBooks
-// Fungsi ini digunakan untuk menampilkan semua buku yang tersimpan
-// Tidak memerlukan parameter
-// Fungsi ini tidak mengembalikan nilai (void)
-// Petunjuk: pikirkan cara menampilkan data buku dengan format yang mudah dibaca
-
-// Fungsi searchBook
-// Fungsi ini digunakan untuk mencari buku berdasarkan judul
-// Parameter title bersifat opsional (bisa ada atau tidak)
-// Fungsi ini tidak mengembalikan nilai (void)
-// Petunjuk: jika parameter title diberikan, cari buku yang cocok
-//           jika tidak diberikan, tampilkan semua buku atau berikan informasi yang sesuai
-
 import { Book } from '../types/index';
 import { books } from '../data/books';
 
-// Fungsi addBook
+// Fungsi addBook digunakan untuk menambahkan buku baru ke dalam koleksi
 export function addBook(newBook: Book): void {
   books.push(newBook);
 
@@ -31,7 +12,7 @@ export function addBook(newBook: Book): void {
   );
 }
 
-// Fungsi ListBooks
+// Fungsi ListBooks digunakan untuk menampilkan semua buku yang ada dalam koleksi
 
 export function listBooks(): void {
   console.log('📚 Daftar Buku:');
@@ -47,4 +28,37 @@ export function listBooks(): void {
   });
 
   console.log('\n================================\n');
+}
+
+// Fungsi searchBook digunakan untuk mencari buku berdasarkan judul, jika parameter title tidak diberikan, maka akan menampilkan semua buku yang tersedia
+
+export function searchBook(title?: string): void {
+  if (title === undefined) {
+    console.log(
+      '\n tidak ada keyword pencarian, menampilkan semua buku yang tersedia...\n'
+    );
+    listBooks();
+    return;
+  }
+
+  console.log(`\n🔍 Mencari buku dengan judul yang mengandung "${title}"...\n`);
+
+  const result: Book[] = books.filter((book) =>
+    book.title.toLowerCase().includes(title.toLowerCase())
+  );
+
+  if (result.length === 0) {
+    console.log(
+      `❌ Tidak ditemukan buku dengan judul yang mengandung "${title}"\n`
+    );
+
+    return;
+  }
+
+  console.log(`✅ Ditemukan ${result.length} buku yang cocok:\n`);
+  result.forEach((book, index) => {
+    console.log(` ${index + 1}. ${book.title}`);
+    console.log(` Penulis: ${book.author}`);
+    console.log(` Tahun: ${book.publicationonYear}\n`);
+  });
 }
